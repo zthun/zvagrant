40857e720c64410a8cb76149460a071992dbbf70
Anthony Bonta <anthony.bonta@trintech.com>
2020-02-11 16:26:25 -0600

    Added vagrantfile.  Added iis.

---
+++ packages/windows-10/src/scripts/install-windows-features.ps1
@@ -0,0 +1,11 @@
+# * Make sure you run this script from a Powershel Admin Prompt!
+# * Make sure Powershell Execution Policy is bypassed to run these scripts:
+# * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
+Set-ExecutionPolicy Bypass -Scope Process
+
+# To list all Windows Features: dism /online /Get-Features
+# Get-WindowsOptionalFeature -Online 
+# LIST All IIS FEATURES: 
+# Get-WindowsOptionalFeature -Online | where FeatureName -like 'IIS-*'
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
